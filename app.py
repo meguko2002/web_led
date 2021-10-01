1c535d0a4ddaa522ab527f24008f845148f6dd77
meguko2002 <meguko2002@gmail.com>
2021-10-01 16:58:58 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,54 @@
+import socket
+from flask import Flask, render_template, request, Response
+from camera import Camera
+import serial.tools.list_ports
+
+# HOST = '172.18.17.111'
+HOST = None
+PORT = 5000
+
+if HOST is not None:
+    host = HOST
+else:
+    host = socket.gethostname()
+
+app = Flask(__name__, static_url_path='/static')
+
+
+def light_ctr(command):
+    valByte = command.to_bytes(1, 'big')
+    with serial.Serial("COM4", 115200) as ser:
+        ser.write(valByte)
+        led_state_str = ser.read()
+        led_state = int.from_bytes(led_state_str, 'big')
+    return led_state
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        led_state = light_ctr(2)    # 引数に0,1以外を入れてLEDの状態をESPから聞き出す
+        msg = 'ON' if led_state else 'OFF'
+        return render_template("index.html", msg=msg)
+    elif request.method == 'POST':
+        req = request.get_data().decode('utf-8')
+        command = 1 if req == 'on' else 0
+        led_state = light_ctr(command)
+        msg = 'ON' if led_state else 'OFF'
+        return msg
+
+
+@app.route('/feed')
+def feed():
+    return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host=host, port=PORT, threaded=True, debug=True)
