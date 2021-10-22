64f8231c1fe502de6b6023af1ade823cb7e69bc1
meguko2002 <meguko2002@gmail.com>
2021-10-22 17:29:11 +0900

    Update app.py
    enable servo control

--- app.py
+++ app.py
@@ -16,19 +16,30 @@ else:
 app = Flask(__name__, static_url_path='/static')
 
 
-def light_ctr(command):
+def light_ctr(command):  # 引数:{0: OFF，1: ON, その他:制御せず}, 戻り値: LEDの状態
     reply_msg = ['turned off', 'turned on', 'no connection']
     valByte = command.to_bytes(1, 'big')
     try:
-        with serial.Serial("COM3", 115200) as ser:
+        with serial.Serial("COM4", 115200) as ser:
             ser.write(valByte)
             led_state_str = ser.read()
             led_state = int.from_bytes(led_state_str, 'big')
     except SerialException:
-        print('no connection with LED')
         led_state = 2
     return reply_msg[led_state]
 
+# todo servoに角度値を送って回動させる
+def servo_ctr(ang):
+    pass
+    # valByte = command.to_bytes(1, 'big')
+    # try:
+    #     with serial.Serial("COM4", 115200) as ser:
+    #         ser.write(valByte)
+    #         led_state_str = ser.read()
+    #         led_state = int.from_bytes(led_state_str, 'big')
+    # except SerialException:
+    #     led_state = 2
+    # return reply_msg[led_state]
 
 def gen(camera):
     while True:
@@ -36,17 +47,40 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
+def is_num(s):
+    try:
+        float(s)
+    except ValueError:
+        return False
+    else:
+        return True
 
+angle = 0
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    global angle
     if request.method == 'GET':
-        msg = light_ctr(0)  # 引数に0,1以外を入れてLEDの状態を変えずにESPからLEDの状態を聞き出す
+        command = 2
+        msg = light_ctr(command)  # LEDをコントロールせずにLEDの状態を見に行く
         return render_template("index.html", msg=msg)
-    elif request.method == 'POST':
-        req = request.get_data().decode('utf-8')
-        command = 1 if req == 'on' else 0
+    if request.method == 'POST':
+        led_req = request.form['switch']
+        angle_str = request.form['ang_val']
+        if is_num(angle_str):
+            angle=int(angle_str)
+            if angle<0:angle=0
+            elif angle>180:angle=180
+            # todo servoを制御
+            servo_ctr(angle)
+            
+        if led_req == 'on':
+            command = 1
+        elif led_req == 'off':
+            command = 0
+        else:
+             command =2
         msg = light_ctr(command)
-        return jsonify({"led": msg})
+        return jsonify({"ledState": msg, "angle":angle})
 
 
 @app.route('/feed')
@@ -55,4 +89,4 @@ def feed():
 
 
 if __name__ == '__main__':
-    app.run(host=host, port=PORT, threaded=True, debug=True)
+    app.run(host=host, port=PORT, debug=True)
